a6aaac60d71f478d5ca401e32b744a784da0cf87
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-18 02:00:37 +0600

    Path navigation added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{ useEffect }  from 'react';
 import Navbar from '../pages/Shared/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const Main = () => {
+    const loc = useLocation()
+    useEffect(()=>{
+      if(loc.pathname === '/'){
+        document.title = "Mighty Marvelous | Home"
+      }else{
+        document.title = `Mighty Marvelous ${loc.pathname.replace("/" , "| ")}`
+      }
+    },[loc.pathname])
     return (
         <section>
             <Navbar />
--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import animationData from "../../assets/animation/92157-404-animation.json";
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const ErrorPage = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='flex items-center justify-center min-h-[calc(100vh-150px)]'>
+            <div>
+               <div className='lg:w-96'>
+               <Lottie animationData={animationData} loop={true} />
+               </div>
+
+               <Link to="/">
+                <div className='text-center mt-5'>
+                <button className='myBtn inline-flex items-center gap-1'><FaArrowLeft /> Back to home</button>
+                </div>
+               </Link>
+            </div>
+        </section>
     );
 };
 
