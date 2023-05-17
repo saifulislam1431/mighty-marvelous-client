4ab475994933a232a8b4829adae74aa821c4d380
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-17 22:18:59 +0600

    Route Setup

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
+const Main = () => {
+    return (
+        <section>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </section>
+    );
+};
+
+export default Main;
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layouts/Main';
+import Home from '../pages/Home/Home/Home';
+import MyToys from '../pages/MyToys/MyToys';
+import AllToys from '../pages/AllToys/AllToys';
+import Blogs from '../pages/Blogs/Blogs';
+import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
+import AddToy from '../pages/AddToy/AddToy';
+import ErrorPage from '../pages/Error/ErrorPage';
 
 const router = createBrowserRouter([
-    
-])
+    {
+        path: "/",
+        element: <Main></Main>,
+        errorElement:<ErrorPage></ErrorPage>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+            {
+                path:"myToys",
+                element:<MyToys></MyToys>
+            },
+            {
+                path:"allToys",
+                element:<AllToys></AllToys>
+            },
+            {
+                path:"addToys",
+                element:<AddToy></AddToy>
+            },
+            {
+                path:"blog",
+                element:<Blogs></Blogs>
+            },
+            {
+                path:"login",
+                element:<Login></Login>
+            },
+            {
+                path:"register",
+                element:<Register></Register>
+            }
+        ]
+      },
+]);
 
 export default router;
